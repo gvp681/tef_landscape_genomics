c4b1aac73c7afddf1563f7019693c9d2b3a251f8
gvp681 <63878806+gvp681@users.noreply.github.com>
2024-04-11 17:33:46 -0600

    Update IBD.R

--- scripts/population_structure/IBD.R
+++ scripts/population_structure/IBD.R
@@ -10,4 +10,8 @@ lat_long_geo <- rdist.earth(data[c(6:7)], miles = FALSE)
 row.names(lat_long_geo) <- data$Sample
 colnames(lat_long_geo) <- data$Sample
 
+plot(prov$dist, prov$g_dist, pch=20, cex=0.5,xlab="Geographic Distance", ylab="Prevosti's Distance")
+image(dens, col = transparentColors, add = TRUE)
+abline(lm(prov$g_dist ~ prov$dist))
+lines(loess.smooth(prov$dist, prov$g_dist), col="red")
 #######NOT DONE###############
